6970c739cca75c037bdefe31da86073bf7b44d95
Jacobo Ruiz <58109748+jacobo-r@users.noreply.github.com>
2025-10-08 16:17:10 -0500

    Create send_ws.bat

---
+++ send_ws.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "D:\USUARIOS (NO BORRAR)\ADMINISTRADOR\Downloads\office-automation-feasibility-test-main"
+"C:\Users\Administrador\AppData\Local\Programs\Python\Python314\python.exe" ws_send_pdf.py ws://127.0.0.1:9000 "D:\USUARIOS (NO BORRAR)\ADMINISTRADOR\Downloads\office-automation-feasibility-test-main\temp_pdf" >> ws_send_log_from_macro.txt 2>&1
